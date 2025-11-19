75140cff5b3be309f26ef6c4abc9601dd3836ce5
albukhorisofyan-ops <albukhorisofyan@gmail.com>
2025-11-19 15:46:02 +0800

    admin

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 $json =file_get_contents('https://gist.githubusercontent.com/ihgoyarp/9327fcd326756747e042bdc499ceef01/raw/235202678a029d072369806a9781b89f36707ab9/SI32_PWD_2025' );
 $data = json_decode($json, TRUE);
 
-var_dump($json);
 ?>
 
 <section>
